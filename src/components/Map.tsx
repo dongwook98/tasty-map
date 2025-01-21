418b6ac12a0c74ece896a6c93fcf13cd09473cd5
강동욱 <ehddnr8563@gmail.com>
2025-01-21 17:35:21 +0900

    feat: 커스텀 마커 설정

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 /*global kakao*/
 
 import Script from 'next/script';
@@ -26,7 +27,20 @@ export default function Map() {
 
       // 식당 데이터 마커 띄우기
       stores?.['DATA'].map((store) => {
-        // 마커가 표시될 위치입니다
+        const imageSrc = store?.bizcnd_code_nm
+          ? `/images/markers/${store?.bizcnd_code_nm}.png`
+          : '/images/markers/default.png';
+        const imageSize = new window.kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
+        const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        const markerImage = new window.kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption
+        );
+
+        // 마커가 표시될 위치 설정
         const markerPosition = new window.kakao.maps.LatLng(
           store?.y_dnts,
           store?.x_cnts
@@ -35,6 +49,7 @@ export default function Map() {
         // 마커를 생성
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
+          image: markerImage, // 마커이미지 설정
         });
 
         // 마커가 지도 위에 표시되도록 설정
