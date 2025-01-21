423df03f67c7f8daa111d163bdbfcea7d6d15c04
강동욱 <ehddnr8563@gmail.com>
2025-01-21 18:24:29 +0900

    refactor: 컴포넌트 분리

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 /*global kakao*/
 
 import Script from 'next/script';
-import * as stores from '@/data/store_data.json';
+import { Dispatch, SetStateAction } from 'react';
 
 declare global {
   interface Window {
@@ -14,7 +13,15 @@ declare global {
 const DEFAULT_LAT = 37.497625203;
 const DEFAULT_LNG = 127.03088379;
 
-export default function Map() {
+interface MapProps {
+  setMap: Dispatch<SetStateAction<any>>;
+}
+
+/**
+ * 카카오 지도 API를 사용해 카카오 지도를 표시하는 컴포넌트
+ * @returns 카카오 지도 Script, 지도 영역
+ */
+export default function Map({ setMap }: MapProps) {
   const loadKaKaoMap = () => {
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById('map');
@@ -25,36 +32,7 @@ export default function Map() {
 
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-      // 식당 데이터 마커 띄우기
-      stores?.['DATA'].map((store) => {
-        const imageSrc = store?.bizcnd_code_nm
-          ? `/images/markers/${store?.bizcnd_code_nm}.png`
-          : '/images/markers/default.png';
-        const imageSize = new window.kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
-        const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-        const markerImage = new window.kakao.maps.MarkerImage(
-          imageSrc,
-          imageSize,
-          imageOption
-        );
-
-        // 마커가 표시될 위치 설정
-        const markerPosition = new window.kakao.maps.LatLng(
-          store?.y_dnts,
-          store?.x_cnts
-        );
-
-        // 마커를 생성
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
-          image: markerImage, // 마커이미지 설정
-        });
-
-        // 마커가 지도 위에 표시되도록 설정
-        marker.setMap(map);
-      });
+      setMap(map);
     });
   };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 import Layout from '@/components/Layout';
 import Map from '@/components/Map';
+import Markers from '@/components/Markers';
+import * as stores from '@/data/store_data.json';
+import StoreBox from '@/components/StoreBox';
 
 export default function Home() {
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeDatas = stores['DATA'];
+
+  console.log(currentStore);
+
   return (
     <Layout>
-      <Map />
+      <Map setMap={setMap} />
+      <Markers
+        storeDatas={storeDatas}
+        map={map}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
     </Layout>
   );
 }
