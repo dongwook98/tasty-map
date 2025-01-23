e63fdabd8c57c550c8a2f61f8e3438ad6fc2e6d2
강동욱 <ehddnr8563@gmail.com>
2025-01-23 20:58:55 +0900

    chore: 주석 추가

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,11 +1,32 @@
 export interface StoreType {
   id: number;
+  /**
+   * 업소 번호
+   */
   phone?: string | null;
+  /**
+   * 업소 주소
+   */
   address?: string | null;
+  /**
+   * 위도
+   */
   lat?: string | null;
+  /**
+   * 경도
+   */
   lng?: string | null;
+  /**
+   * 업소명
+   */
   name?: string | null;
+  /**
+   * 업태명
+   */
   category?: string | null;
+  /**
+   * 업종명
+   */
   storeType?: string | null;
   foodCertifyName?: string | null;
 }
